efd1d4558313163731a5ef9c138c88be6ebba7e1
Muhammed Efe Çetin <efectn@protonmail.com>
2021-09-24 18:24:50 +0300

    Catch interrupt.

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/efectn/fiber-boilerplate/internal"
 	"github.com/efectn/fiber-boilerplate/internal/config"
 	"github.com/efectn/fiber-boilerplate/internal/routes"
 )
+
+func init() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\n\u001b[96msee you again👋\u001b[0m")
+		os.Exit(1)
+	}()
+}
 
 func main() {
 	config, err := config.ParseConfig("example")
